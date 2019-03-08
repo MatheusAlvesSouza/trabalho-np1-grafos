205c4cd00bc1efaa1801359b546f80b56342e5fa
Matheus Alves <matheus.alves07@outlook.com>
2019-03-08 14:45:23 -0300

    Adicionado exceptions

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 import naoDirigido as grafosNaoDirigidos
 import dirigido as grafosDirigidos
 
-print('________________________')
-print('0 - Grafo não dirigido')
-print('1 - Grafo dirigido')
-print('________________________')
-print('Selecione uma opção :', end=" ")
-dirigido = int(input())
+def inicio():
+    print('O Conversor de grafos 1.0')
+    print('_________________________')
+    print('0 - Grafo não dirigido')
+    print('1 - Grafo dirigido')
+    print('_________________________')
+    print('Selecione uma opção :', end=" ")
+    dirigido = int(input())
 
-print('________________________')
-print('0 - O caminho não tem peso')
-print('1 - O caminho tem peso')
-print('________________________')
-print('Selecione uma opção :', end=" ")
-peso = int(input())
+    print('_________________________')
+    print('0 - O caminho não tem peso')
+    print('1 - O caminho tem peso')
+    print('_________________________')
+    print('Selecione uma opção :', end=" ")
+    peso = int(input())
 
-if dirigido == 0 and peso == 0:
-    grafosNaoDirigidos.sem_peso()
-elif dirigido == 0 and peso == 1:
-    grafosNaoDirigidos.com_peso()
-elif dirigido == 1 and peso == 0:
-    grafosDirigidos.sem_peso()
-elif dirigido == 1 and peso == 1:
-    grafosDirigidos.com_peso()
-else:
-    print("Opção inválida!")
+    if dirigido == 0 and peso == 0:
+        grafosNaoDirigidos.sem_peso()
+    elif dirigido == 0 and peso == 1:
+        grafosNaoDirigidos.com_peso()
+    elif dirigido == 1 and peso == 0:
+        grafosDirigidos.sem_peso()
+    elif dirigido == 1 and peso == 1:
+        grafosDirigidos.com_peso()
+    else:
+        print("Opção inválida !")
+        inicio()
+        
+try:
+    inicio()
+except:
+    inicio()
